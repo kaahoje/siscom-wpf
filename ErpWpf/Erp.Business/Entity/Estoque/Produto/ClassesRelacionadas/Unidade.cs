e60631b0d4d3687c72f77feac0cc9f13d4851d5b
kaahoje <kaahoje@45382a2e-173a-df9f-30af-e124757937f8>
2014-03-18 06:30:22 +0000

    Corerção do cadastro de unidades, com a implementação das annotations na classe de negócios.

--- ErpWpf/Erp.Business/Entity/Estoque/Produto/ClassesRelacionadas/Unidade.cs
+++ ErpWpf/Erp.Business/Entity/Estoque/Produto/ClassesRelacionadas/Unidade.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
 using Erp.Business.Enum;
 
 namespace Erp.Business.Entity.Estoque.Produto.ClassesRelacionadas
@@ -7,8 +9,16 @@ namespace Erp.Business.Entity.Estoque.Produto.ClassesRelacionadas
     public class Unidade
     {
         public virtual int Id { get; set; }
+        [Display(Name = "Sigla", Description = "Sigla da unidade no S.I. ou padrão da empresa.")]
+        [Required(ErrorMessage = Constants.MessageRequiredError)]
         public virtual string Sigla { get; set; }
+        [Display(Name = "Descrição",Description = "Descrição da unidade")]
+        [Required(ErrorMessage = Constants.MessageRequiredError)]
+        [StringLength(Constants.MaxLengthNames,MinimumLength = Constants.MinLengthNames, ErrorMessage = Constants.MessageLengthNameError)]
         public virtual string Descricao { get; set; }
+        [Display(Name = "Quantidade",Description = "Quantidade movimentada por uma unidade.")]
+        [Required(ErrorMessage = Constants.MessageRequiredError)]
+        [Range(Constants.MinQuantidade,Constants.MaxQuantidade,ErrorMessage = Constants.MessageRangeError)]
         public virtual decimal Quantidade { get; set; }
         public virtual Status Status { get; set; }
     }

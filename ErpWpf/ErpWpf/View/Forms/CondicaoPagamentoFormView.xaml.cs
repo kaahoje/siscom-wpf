f7e2f1e5db9667cca543856d32b9b36a3f1f4712
kaahoje <kaahoje@45382a2e-173a-df9f-30af-e124757937f8>
2014-03-18 07:40:19 +0000

    Ajuste do modelo de condições de pagamento para trabalhar os prazos, construída a base da tela que receberá a entrada dos dados das condições de pagamento.

--- ErpWpf/ErpWpf/View/Forms/CondicaoPagamentoFormView.xaml.cs
+++ ErpWpf/ErpWpf/View/Forms/CondicaoPagamentoFormView.xaml.cs
@@ -1,20 +1,13 @@
-﻿using Erp.Business.Entity.Vendas.Pedido.ClassesRelacionadas;
-using Erp.Model.Forms;
-
-namespace Erp.View.Forms
+﻿namespace Erp.View.Forms
 {
     /// <summary>
     /// Interaction logic for CondicaoPagamentoFormView.xaml
     /// </summary>
     public partial class CondicaoPagamentoFormView 
     {
-        private FormDefaultActions<CondicaoPagamento> FormDefaultActions { get; set; }
         public CondicaoPagamentoFormView()
         {
             InitializeComponent();
-            DataContext = new CondicaoPagamentoFormModel();
-            RestCommands.DataContext = DataContext;
-            FormDefaultActions = new FormDefaultActions<CondicaoPagamento>(this);
         }
     }
 }

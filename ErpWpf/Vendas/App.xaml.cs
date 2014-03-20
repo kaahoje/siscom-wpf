ae1d4865014fe474106afcf708438a3460e4b51b
kaahoje <kaahoje@45382a2e-173a-df9f-30af-e124757937f8>
2014-03-20 05:54:34 +0000

    Ajustes finais para distribuição.

--- ErpWpf/Vendas/App.xaml.cs
+++ ErpWpf/Vendas/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.Windows;
 using Ecf;
 using Erp.Business.Entity.Contabil.Pessoa.SubClass.PessoaFisica;
@@ -6,6 +7,7 @@ using Erp.Business.Entity.Contabil.Pessoa.SubClass.PessoaJuridica;
 using Util;
 using Vendas.Enums;
 using Vendas.Properties;
+using MessageBox = System.Windows.Forms.MessageBox;
 
 namespace Vendas
 {
@@ -18,24 +20,32 @@ namespace Vendas
         public static PessoaJuridica Proprietaria { get; set; }
         protected override void OnStartup(StartupEventArgs e)
         {
-            base.OnStartup(e);
-            var processName = Process.GetCurrentProcess().ProcessName;
+            try
+            {
+                base.OnStartup(e);
+                var processName = Process.GetCurrentProcess().ProcessName;
 
-            if (Process.GetProcessesByName(processName).Length > 1)
-            {
-                CustomMessageBox.MensagemCritica("Já existe uma instancia do aplicativo rodando.\n\nA aplicação será encerrada.");
-                Process.GetCurrentProcess().Kill();
+                if (Process.GetProcessesByName(processName).Length > 1)
+                {
+                    CustomMessageBox.MensagemCritica("Já existe uma instancia do aplicativo rodando.\n\nA aplicação será encerrada.");
+                    Process.GetCurrentProcess().Kill();
+                }
+                EcfHelper.FabricanteEcf = Settings.Default.FabricanteEcf;
+                Usuario = PessoaFisicaRepository.GetByLogin("admin");
+                Proprietaria = PessoaJuridicaRepository.GetById(Settings.Default.IdEmpresa);
+                switch (Settings.Default.TipoPdv)
+                {
+                    case TipoPdv.Mercearia:
+                        break;
+                    case TipoPdv.Restaurante:
+                        new RestauranteWindow().Show();
+                        break;
+                }
+            
             }
-            EcfHelper.FabricanteEcf = Settings.Default.FabricanteEcf;
-            Usuario = PessoaFisicaRepository.GetByLogin("admin");
-            Proprietaria = PessoaJuridicaRepository.GetById(Settings.Default.IdEmpresa);
-            switch (Settings.Default.TipoPdv)
+            catch (Exception ex)
             {
-                case TipoPdv.Mercearia:
-                    break;
-                case TipoPdv.Restaurante:
-                    new RestauranteWindow().Show();
-                    break;
+                MessageBox.Show(ex.Message + "\n" + ex.StackTrace);
             }
             
         }
